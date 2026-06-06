fdeb6698ecaa3e63968df7a3ece02f63a8ce2760
Mario Leonhardt <marioleonardt@gmail.com>
2026-06-06 10:54:50 +0100

    fix types

--- personabox/lib/types.ts
+++ personabox/lib/types.ts
@@ -4,19 +4,6 @@ export interface ContentItem {
   addedAt: string
 }
 
-export interface ImageItem {
-  data: string      // base64 data URL (compressed JPEG)
-  name: string
-  addedAt: string
-}
-
-export interface VoiceItem {
-  audioData: string  // base64 data URL (audio/webm)
-  duration: number   // seconds
-  createdAt: string
-  sentNote?: string  // z.B. "LinkedIn 05.06.26"
-}
-
 export interface Persona {
   id: string
   firstname: string
@@ -24,7 +11,6 @@ export interface Persona {
   name: string
   gender: string
   email: string
-  mobile: string
   title: string
   company: string
   industry: string
@@ -35,11 +21,7 @@ export interface Persona {
   privateGoal: string
   product: string
   keywords: string[]
-  linkedinHeadline: string
-  linkedinInfo: string
   contentItems: ContentItem[]
-  imageItems: ImageItem[]
-  voiceItems: VoiceItem[]
   analysis?: string
   created_at?: string
 }
